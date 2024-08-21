b3a0f1becbb4074c56c68923a3a8ba37604159bf
morfidon <armiksos@gmail.com>
2024-08-21 18:49:19 +0200

    GoldMinerUpgrade

--- src/components/GoldMiner.jsx
+++ src/components/GoldMiner.jsx
@@ -3,15 +3,28 @@ import { useState } from 'react'
 function GoldMiner()
 {
     const [gold, setGold] = useState(0);
+    const [clickPower, setClickPower] = useState(1);
+    const [upgradePowerCost, setUpgradePowerCost] = useState(10);
+
+    const upgradeClickPower = () =>
+    {
+        if (gold >= upgradePowerCost)
+        {
+            setGold(gold - upgradePowerCost);
+            setClickPower(clickPower + 1);
+            setUpgradePowerCost(upgradePowerCost * 2);
+        }
+    }
 
     return (
         <div>
             <h1>Gold Miner!</h1>
             <p>Gold: {gold}</p>
-
+            <p>Mining level: {clickPower} </p>
             <button onClick={() => {
-                setGold(gold + 1);
+                setGold(gold + clickPower);
             }}>Mine Gold</button>
+            <button onClick={upgradeClickPower}>Upgrade Click Power (Cost: {upgradePowerCost} Gold)</button>
         </div>
     );    
 }
