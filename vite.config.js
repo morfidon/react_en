06b55c02d5710c30b2e2f901f90de1096c5effe3
morfidon <armiksos@gmail.com>
2024-09-02 13:53:49 +0200

    pwa icons

--- vite.config.js
+++ vite.config.js
@@ -15,14 +15,28 @@ export default defineConfig({
         "start_url": "./",
         "icons": [
           {
-            "src": "icon.png",
+            "src": "icons/manifest-icon-192.maskable.png",
             "sizes": "192x192",
-            "type": "image/png"
+            "type": "image/png",
+            "purpose": "any"
           },
           {
-            "src": "icon.png",
+            "src": "icons/manifest-icon-192.maskable.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "icons/manifest-icon-512.maskable.png",
             "sizes": "512x512",
-            "type": "image/png"
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "icons/manifest-icon-512.maskable.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
           }
         ],
       }
