d8332d972bd81e1b7edfd2488f1bc7269c11b54d
morfidon <armiksos@gmail.com>
2024-09-02 12:56:11 +0200

    pwa init

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        "name": "Auto Clicker",
+        "short_name": "AutoClicker",
+        "display": "standalone",
+        "start_url": "./",
+        "icons": [
+          {
+            "src": "icon.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "icon.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ],
+      }
+    })
+  ],
   base: './'
 })
