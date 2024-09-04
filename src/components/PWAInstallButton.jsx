8ccfa4a47fe2840401a42d12c24d5eee567a8fad
morfidon <armiksos@gmail.com>
2024-09-04 12:37:46 +0200

    resolve promise

--- src/components/PWAInstallButton.jsx
+++ src/components/PWAInstallButton.jsx
@@ -1,22 +1,37 @@
 import { useEffect, useState } from 'react';
 function PWAInstallButton() {
     const [showInstallButton, setShowInstallButton] = useState(false);
+    const [deferredPrompt, setDeferredPrompt] = useState(null);
 
     useEffect(() => {
 
         const handleBeforeIntallPrompt = (event) => {
             console.log('beforeinstallprompt event fired');
             event.preventDefault();
+            setDeferredPrompt(event);
             setShowInstallButton(true);
         }
 
         window.addEventListener('beforeinstallprompt', handleBeforeIntallPrompt);
         return () => window.removeEventListener('beforeinstallprompt', handleBeforeIntallPrompt)
     }, [])
- 
+    
+    const handleInstallClick = async () => {
+        if (deferredPrompt) 
+        {
+            const choiceResult = await deferredPrompt.prompt(); //Promise object
+
+            if (choiceResult.outcome === 'accepted') 
+            {
+                setDeferredPrompt(null);
+                setShowInstallButton(false);
+            }
+
+        }
+    }
     return(
         showInstallButton ?
-        <button>
+        <button onClick={handleInstallClick}>
           Instal PWA app
         </button> : null
     )
