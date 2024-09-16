0e5ea7bc21561c86052d9e6f884b7ec42feb71f3
morfidon <armiksos@gmail.com>
2024-09-16 14:51:07 +0200

    Express init

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import './App.css'
-import ClassicToggleButton from './components/ClassicToggleButton';
-import GoldMiner from './components/GoldMiner'
-import PWAInstallButton from './components/PWAInstallButton';
+
 import ThemeToggleButton from './components/ThemeToggleButton';
 import { ThemeProvider } from './contexts/ThemeContext';
+
 function App()  //THIS IS COMPONENT 
 {
     
     return (
       <>
         <ThemeProvider>
-          <GoldMiner />
+        
           <ThemeToggleButton />
-          <ClassicToggleButton />
-          <PWAInstallButton />
+          
         </ThemeProvider>
         
       </>
