9fdc9a9bf7ddb3767b9d0780d09e2dd657e1698f
morfidon <armiksos@gmail.com>
2024-09-16 15:24:55 +0200

    setting-up-express-setup

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+
+app.get('/api/hello', //name of endpoint for your server to get reponse
+    function (req, res) //req - request  (what comes to me), res (response - what I send)
+    {
+        res.send('Hello World')
+    }
+)
+
+app.listen(3000)
